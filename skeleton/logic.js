099b619db43fa46902403e218997060a23a57e55
mohamadD <rediab88@gmail.com>
2019-09-24 16:06:55 +0300

    add delete function

--- skeleton/logic.js
+++ skeleton/logic.js
@@ -24,15 +24,20 @@ var todoFunctions = {
   },
 
   addTodo: function(todos, newTodo) {
+    var newTodos = this.cloneArrayOfObjects(todos);
+    var newItem = [{ id: this.generateId(), description: newTodo }];
+    return newTodos.concat(newItem);
     // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
     // returns a new array, it should contain todos with the newTodo added to the end.
     // add an id to the newTodo. You can use the generateId function to create an id.
     // hint: array.concat
   },
   deleteTodo: function(todos, idToDelete) {
-    // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
-    // return a new array, this should not contain any todo with an id of idToDelete
-    // hint: array.filter
+    var newTodos = this.cloneArrayOfObjects(todos);
+     newTodos = newTodos.filter(item => item.id != idToDelete);
+return newTodos;
+
+    
   },
   markTodo: function(todos, idToMark) {
     // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
